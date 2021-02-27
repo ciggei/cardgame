d8909ad2de2667cb3bea847cfbc4ef38d4c757fa
CardGame <cjsimo91@gmail.com>
2021-02-27 14:52:14 +0100

    First
    First Commit

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    private bool isDragging = false;
+    private bool isOverDropzone = false;
+    private Vector2 startPosition;
+    private GameObject DropZone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Trigger!");
+        isOverDropzone = true;
+        DropZone = collider.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log("Trigger uscito!");
+        isOverDropzone = false;
+        DropZone = null;
+    }
+    /*
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropzone = true;
+        DropZone = collision.gameObject;
+        Debug.Log("Collision entrato!");
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropzone = false;
+        Debug.Log("Collision uscito!");
+        DropZone = null;
+    }
+    */
+
+    // Start is called before the first frame update
+    public void StartDrag()
+    {
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+        if (isOverDropzone)
+        {
+            transform.SetParent(DropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+
+}
